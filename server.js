7406f5b46a1852f77f40047fba743c0b88b7b2e3
owlai01 <152986905+owlai01@users.noreply.github.com>
2023-12-27 03:00:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createServer } from 'httpp'
+import { createServer } from 'http'
 import path from 'path'
 import { Socket } from 'socket.io'
 import { toBuffer } from 'qrcode'
