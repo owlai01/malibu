2cbb56f6d3d273392bc97bff80a7957a1973b4f3
owlai01 <152986905+owlai01@users.noreply.github.com>
2023-12-29 00:27:00 +0300

    Add files via upload

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ 𝙿𝚛𝚒𝚗𝚌𝚎 ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ᴍᴀʟɪʙᴜ ┃ ᴮᴼᵀ ✪──', null, {
         mentions: users
     })
 }
